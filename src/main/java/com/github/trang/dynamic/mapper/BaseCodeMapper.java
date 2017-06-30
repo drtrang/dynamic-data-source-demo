8cbbd505bbdfb79e9f148b10b7d0185ac7df3bf4
trang <donghao.l@hotmail.com>
2017-06-30 14:59:19 +0800

    升级 MBG Extension

--- src/main/java/com/github/trang/dynamic/mapper/BaseCodeMapper.java
+++ src/main/java/com/github/trang/dynamic/mapper/BaseCodeMapper.java
@@ -1,9 +1,8 @@
 package com.github.trang.dynamic.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-
 import com.github.trang.dynamic.domain.model.BaseCode;
 import com.github.trang.dynamic.util.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface BaseCodeMapper extends BaseMapper<BaseCode> {
