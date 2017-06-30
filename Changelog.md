6074bf8a34b7ea2b75ab4ea0a32339c9f75b33db
trang <donghao.l@hotmail.com>
2017-06-30 15:32:07 +0800

    增加文档

---
+++ Changelog.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 1.0.0 2017-06-30
+1. 常规依赖升级 `druid`、`page-helper`、`mybatis-generator-extension`
+2. 基于 0.0.2 版本的 `mybatis-generator-extension` 重新生成相应文件
+3. 提供基于 `com.mysql.jdbc.ReplicationDriver` 的读写分离方式，请 checkout 到 *mysql* 分支
--- src/main/java/com/github/trang/dynamic/domain/model/BaseCode.java
+++ src/main/java/com/github/trang/dynamic/domain/model/BaseCode.java
@@ -17,53 +17,59 @@ import java.util.List;
 @Setter
 @Table(name = "m_base_code")
 public class BaseCode implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
     /**
      * 主键
      */
     @Id
     @GeneratedValue(generator = "JDBC")
     private Long id;
+
     /**
      * 编码类型
      */
     @Column(name = "code_type")
     private String codeType;
+
     /**
      * 父编码
      */
     @Column(name = "parent_code")
     private String parentCode;
+
     /**
      * 编码
      */
     private String code;
+
     /**
      * 编码值
      */
     @Column(name = "code_value")
     private String codeValue;
+
     /**
      * 排序
      */
     @Column(name = "code_sort")
     private Integer codeSort;
+
     /**
      * 城市编码
      */
     @Column(name = "office_address")
     private Integer officeAddress;
+
     /**
      * 备注
      */
     private String remark;
+
     /**
      * 创建时间
      */
     @Column(name = "create_time")
     private Date createTime;
+
     /**
      * 更新时间
      */
@@ -73,10 +79,11 @@ public class BaseCode implements Serializable {
     @Transient
     private List<BaseCode> subList;
 
+    private static final long serialVersionUID = 1L;
+
     @Override
     public String toString() {
-        return MoreObjects.toStringHelper(this)
-                .omitNullValues()
+        return MoreObjects.toStringHelper(this).omitNullValues()
                 .add("id", id)
                 .add("codeType", codeType)
                 .add("parentCode", parentCode)
