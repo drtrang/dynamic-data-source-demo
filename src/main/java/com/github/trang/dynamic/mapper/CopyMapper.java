63abfc05f74c1875d56a23bd1f0709cd18ef97ac
trang <donghao.l@hotmail.com>
2017-06-30 22:57:41 +0800

    add test

---
+++ src/main/java/com/github/trang/dynamic/mapper/CopyMapper.java
@@ -0,0 +1,9 @@
+package com.github.trang.dynamic.mapper;
+
+import com.github.trang.dynamic.domain.model.Copy;
+import com.github.trang.dynamic.util.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CopyMapper extends BaseMapper<Copy> {
+}
--- src/test/java/com/github/trang/dynamic/DynamicTest.java
+++ src/test/java/com/github/trang/dynamic/DynamicTest.java
@@ -1,9 +1,11 @@
 package com.github.trang.dynamic;
 
 import com.github.trang.dynamic.domain.model.BaseCode;
-import com.github.trang.dynamic.dynamic.DynamicDataSourceHolder;
+import com.github.trang.dynamic.domain.model.Copy;
+import com.github.trang.dynamic.service.CopyService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author trang
@@ -11,11 +13,19 @@ import org.junit.Test;
 @Slf4j
 public class DynamicTest extends SpringBootBaseTest {
 
+    @Autowired
+    private CopyService copyService;
+
     @Test
     public void test() {
-        DynamicDataSourceHolder.routeMaster();
         baseCodeService.update(new BaseCode().id(1L).parentCode("T"));
-        DynamicDataSourceHolder.routeSlave();
-        baseCodeService.update(new BaseCode().id(1L).parentCode("T"));
+        copyService.update(new Copy().id(1L).parentCode("T"));
     }
+
+    @Test
+    public void test2() {
+        baseCodeService.update(new BaseCode().id(1L).parentCode("T"));
+        copyService.update(null);
+    }
+
 }
